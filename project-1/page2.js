94cde51d9cc5114baeea4dda42adb618389e05f8
anushkavhatkar <vhata986@newschool.edu>
2023-02-18 15:29:21 -0500

    page 2: toggle js paragraphs trial

--- project-1/page2.js
+++ project-1/page2.js
@@ -11,3 +11,34 @@
 //     p.innerHTML += "<br><br>Then I reflected that all things happen, happen to one, precisely now. Century follows century, and things happen only in the present. There are countless men in the air, on land and at sea, and all that really happens happens to me... The almost unbearable memory of Madden's long horseface put an end to these wandering thoughts.";
 //  };
 
+//  var divOne = document.getElementById('divone');
+//  var divTwo = document.getElementById('divtwo');
+//  var divThree = document.getElementById('divthree');
+//  var divFour = document.getElementById('divfour');
+
+
+
+//  divOne.onclick = () => {
+//     if (divTwo.style.display !== "none") {
+//         divTwo.style.display = "none";
+//     } else {
+//         divTwo.style.display = "block";
+//     }
+//  };
+
+//  divTwo.onclick = () => {
+//     if (divThree.style.display !== "none") {
+//         divThree.style.display = "none";
+//     } else {
+//         divThree.style.display = "block";
+//     }
+//  };
+
+
+//  divThree.onclick = () => {
+//     if (divFour.style.display !== "none") {
+//         divFour.style.display = "none";
+//     } else {
+//         divFour.style.display = "block";
+//     }
+// }; 
