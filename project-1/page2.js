ebd660b745147c66c48986aef4c3fc11641481b9
anushkavhatkar <vhata986@newschool.edu>
2023-02-18 15:28:55 -0500

    page 2: initial js trial

---
+++ project-1/page2.js
@@ -0,0 +1,13 @@
+// TEST JAVASCRIPT:
+
+// const section = document.querySelector('section');
+// const p = document.querySelector('p');
+
+// p.onclick = () => {
+//    p.innerHTML += "<br><br>I went up to my bedroom. Absurd though the gesture was, I closed and locked the door. I threw myself down on my narrow iron bed, and waited on my back. The never changing rooftops filled the window, and the hazy six o'clock sun hung in the sky. It seemed incredible that this day, a day without warnings or omens, might be that of my implacable death. In despite of my dead father, in despite of having been a child in one of the symmetrical gardens of Hai Feng, was I to die now?";
+// };
+
+// p.onclick = () => {
+//     p.innerHTML += "<br><br>Then I reflected that all things happen, happen to one, precisely now. Century follows century, and things happen only in the present. There are countless men in the air, on land and at sea, and all that really happens happens to me... The almost unbearable memory of Madden's long horseface put an end to these wandering thoughts.";
+//  };
+
