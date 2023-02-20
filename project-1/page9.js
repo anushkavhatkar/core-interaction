00f44f1d47f9901353948b14e962be7acb2742d3
anushkavhatkar <vhata986@newschool.edu>
2023-02-20 15:47:00 -0500

    page9: js final (should be looped through)

--- project-1/page9.js
+++ project-1/page9.js
@@ -11,17 +11,93 @@ myArray = [one, two, three, four, five, six, seven];
 
 console.log(myArray[0]);
 
-for (i=0; i<myArray.length; i++ ) {
-    for (j=1; j<myArray.length; j++) {
-    myArray[i].onclick = () => {
-        if (myArray[j].style.display !== "none") {
-            myArray[j].style.display = "none";
-        } else {
-            myArray[j].style.display = "block";
-        }
-    }
+
+
+myArray[0].onclick = function addSection() {
+    if (myArray[1].style.display !== "none") {
+        myArray[1].style.display = "none";
+    } else { 
+        myArray[1].style.display = "block";
     }
 };
+
+myArray[1].onclick = function addSection() {
+    if (myArray[2].style.display !== "none") {
+        myArray[2].style.display = "none";
+    } else { 
+        myArray[2].style.display = "block";
+    }
+};
+
+myArray[2].onclick = function addSection() {
+    if (myArray[3].style.display !== "none") {
+        myArray[3].style.display = "none";
+    } else { 
+        myArray[3].style.display = "block";
+    }
+};
+
+myArray[3].onclick = function addSection() {
+    if (myArray[4].style.display !== "none") {
+        myArray[4].style.display = "none";
+    } else { 
+        myArray[4].style.display = "block";
+    }
+};
+
+myArray[4].onclick = function addSection() {
+    if (myArray[5].style.display !== "none") {
+        myArray[5].style.display = "none";
+    } else { 
+        myArray[5].style.display = "block";
+    }
+};
+
+myArray[5].onclick = function addSection() {
+    if (myArray[6].style.display !== "none") {
+        myArray[6].style.display = "none";
+    } else { 
+        myArray[6].style.display = "block";
+    }
+};
+
+myArray[6].onclick = function addSection() {
+    if (myArray[7].style.display !== "none") {
+        myArray[7].style.display = "none";
+    } else { 
+        myArray[7].style.display = "block";
+    }
+};
+
+
+
+
+// for (i = 0; i<myArray.length; i++ ) {
+//     for (j = 0; j<myArray.length; j++) {
+//     myArray[i].onclick = () => {
+//         if (myArray[j].style.display !== "none") {
+//             myArray[j].style.display = "none";
+//         } else {
+//             myArray[j].style.display = "block";
+//         }
+//     }
+//     }
+// };
+
+
+
+// for (i=0; i< myArray.length; i++) {
+//     myArray[i].onclick = function addSection() {
+//         if (myArray[i+1].style.display !== "none") {
+//             myArray[i+1].style.display = "none";
+//         } else { 
+//             myArray[i+1].style.display = "block";
+//         }
+//     };
+// }
+
+
+
 
 
 
